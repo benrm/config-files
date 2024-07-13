6688161af8d9893c8a946ca5cfd051c725832f62
Benjamin Magee <bmagee13@gmail.com>
2024-07-13 17:54:19 -0400

    Add script to deploy files

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+CONFIG_DIR="$XDG_CONFIG_HOME"
+if [ -z "$CONFIG_DIR" ] ; then
+	CONFIG_DIR="$HOME/.config"
+fi
+
+find alacritty/ polybar/ xmonad/ -type f | while read -r target ; do
+	install -b -D "$target" "$CONFIG_DIR/$target"
+done
+
+find bin/ -type f | while read -r script ; do
+	install -b "$script" "$HOME/$script"
+done
+
+find dotfiles/ -type f | sed -e 's/^dotfiles\///' | while read -r dotfile ; do
+	install -b  "dotfiles/$dotfile" "$HOME/.$dotfile"
+done
