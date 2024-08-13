cd993843f7f355034058d737c3e163816cc760a0
Benjamin Magee <bmagee@barracuda.com>
2024-08-13 13:09:38 -0400

    Don't do anything fancy with polybar when reloading and add systray

--- files/bin/reload.sh
+++ files/bin/reload.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 set -e
 
-PIDDIR=~/.pids
-
-mkdir -p "$PIDDIR"
-
-PIDFILE="$PIDDIR/polybar.pid"
-
+pkill polybar
 polybar &
-NEWPID="$!"
-
-sleep 1
-
-if [ -e "$PIDFILE" ] ; then
-	read PID < "$PIDFILE"
-	kill -9 "$PID" || echo "Failed to kill Polybar $PID as specified in $PIDFILE"
-fi
-
-echo "$NEWPID" > "$PIDFILE"
